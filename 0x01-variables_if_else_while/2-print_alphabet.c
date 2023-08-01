fe28cec532e77c1199fae4cbcf7cbb0ef686d960
yahya <yahyasahnoun077@example.com>
2023-08-01 05:10:15 -0700

    task done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description : Write a program that prints the alphabet in lowercase
+ * Return : always 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
