44d867b0e2090df13e84ad9d743a479f33665386
yahya <yahyasahnoun077@example.com>
2023-08-01 06:11:16 -0700

    task done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:'prints the lowercase alphabet in reverse'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n == 9; n >= 0; n--)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+
+}
