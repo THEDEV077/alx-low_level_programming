42bd2f6a10fe578f7b963aa424d26f0b7ce42972
yahya <yahyasahnoun077@example.com>
2023-08-01 05:57:53 -0700

    task done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: 'prints all single digit numbers of base 10'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
