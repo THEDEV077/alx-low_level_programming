9169ccd85deb64316a0cad2b608a5d0ad54cf046
yahya <yahyasahnoun077@example.com>
2023-08-01 05:34:55 -0700

    task done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description : prints the alphabet in lowercase
+ * return : always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 133 || n == 101)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+
+}
